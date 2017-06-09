82ca811be88e2c2c0b10b818add6cb96f7a12810
Bernard Williams <bernard.l.williams@live.com>
2017-06-08 21:15:44 -0400

    initial commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,16 @@ $(document).ready(function () {
 
 
     function search(searchTerm, limit, startYear, endYear) {
-
-        var begin = startYear +'0101';
-        var end = endYear +'1231';
+        if (!startYear) {
+            startYear = 2017;
+        }
+        ;
+        if (!endYear) {
+            endYear = 2017;
+        }
+        ;
+        var begin = startYear + '0101';
+        var end = endYear + '1231';
         var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
         url += '?' + $.param({
                 'api-key': "f62d55179eec4db1a761790c6d6d7173",
@@ -30,7 +37,7 @@ $(document).ready(function () {
         });
 
 
-};
+    };
 
     search('UCF');
 });
