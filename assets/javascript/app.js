92dc861697f1b37cecba35f3344082fb0f57c0ae
Bernard Williams <bernard.l.williams@live.com>
2017-06-08 21:09:34 -0400

    initial commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,36 @@
 //Team Tina Code
 //
 
+var articles = [];
 
 $(document).ready(function () {
 
-    function search(searchTerm)
-// Built by LucyBot. www.lucybot.com
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-            'api-key': "f62d55179eec4db1a761790c6d6d7173"
+
+    function search(searchTerm, limit, startYear, endYear) {
+
+        var begin = startYear +'0101';
+        var end = endYear +'1231';
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({
+                'api-key': "f62d55179eec4db1a761790c6d6d7173",
+                q: searchTerm,
+                begin_date: begin,
+                end_date: end
+            });
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function (result) {
+            console.log(result);
+            articles = result.response.docs;
+            console.log(articles);
+        }).fail(function (err) {
+            throw err;
         });
-    $.ajax({
-        url: url,
-        method: 'GET',
-    }).done(function(result) {
-        console.log(result);
-    }).fail(function(err) {
-        throw err;
-    });
+
+
+};
+
+    search('UCF');
 });
